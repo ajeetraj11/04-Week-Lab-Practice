4f61a58cbfe12690c017d72448eca28b265da0db
ajeetraj11 <ajitpandey110920@gmail.com>
2025-02-13 16:53:36 +0530

    [Ajeet Raj] Add. Solution of Personalised Meal Generator

---
+++ Day01-Lab-Practice/com/personalisedmealgenerator/MealPlan.java
@@ -0,0 +1,7 @@
+package com.personalisedmealgenerator;
+
+// Defining an interface to represent a Meal Plan
+interface MealPlan {
+    // Abstract method to get meal details
+    String getMealDetails();
+}
---
+++ Day01-Lab-Practice/com/personalisedmealgenerator/VeganMeal.java
@@ -0,0 +1,18 @@
+package com.personalisedmealgenerator;
+
+// Defining a class for Vegan Meals implementing MealPlan
+public class VeganMeal implements MealPlan {
+    // Declaring a private variable to store the key protein source
+    private String proteinSource;
+
+    // Constructor to initialize the vegan meal with a protein source
+    public VeganMeal(String proteinSource) {
+	   this.proteinSource = proteinSource;
+    }
+
+    // Implementing the method to return details of the vegan meal
+    @Override
+    public String getMealDetails() {
+	   return "Vegan Meal with Protein Source: " + proteinSource;
+    }
+}
